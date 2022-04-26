d09dca391fe13bb65df3f631df426b3e2c815b80
Sebazt <juan.londono29@udea.edu.co>
2022-04-26 15:52:33 -0500

    Server ok

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const express = require('express')
 const PORT = 4000
 
 const app = express()
-app.listen(PORT, () => console.log('Server Listo en el puerto ' + PORT))
+app.listen(PORT, () => console.log('Server Listo en el puerto  ' + PORT))
