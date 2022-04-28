12170c9cb2f9cf099367d03f519631a371d25365
Sebazt <juan.londono29@udea.edu.co>
2022-04-28 12:51:36 -0500

    fuente establecida y svg service

--- frontend/src/Pages/Portfolio/Portfolio.jsx
+++ frontend/src/Pages/Portfolio/Portfolio.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import './portfolio.css'
 
 function Portfolio(){
   return(
-    <>
+    <div id='div__portfolio-ppal'>
     <h1>Hola soy portfolio</h1>
-    </>
+    </div>
   )
 }
 export default Portfolio
