7f623e31669c3cba29e92a8339f6f92e7984b08f
Sebazt <juan.londono29@udea.edu.co>
2022-04-28 13:22:51 -0500

    pagina servicies comenzada, header ok

--- frontend/src/Pages/Portfolio/Portfolio.jsx
+++ frontend/src/Pages/Portfolio/Portfolio.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-import './portfolio.css'
+import React from "react";
+import "./portfolio.css";
+import SvgService from "../../Media/services.svg";
 
-function Portfolio(){
-  return(
-    <div id='div__portfolio-ppal'>
-    <h1>Hola soy portfolio</h1>
-    </div>
-  )
+function Portfolio() {
+  return (
+    <>
+      <div className="div__portfolio-titulo">
+        <h1>Nuestro Portafolio</h1>
+      </div>
+
+      <div id="div__portfolio-ppal">
+        <img src={SvgService} alt="Equipo de trabajo álom" />
+        <div className="div__portfolio-logo">
+          <img src="" alt="" />
+        </div>
+      </div>
+
+      <div className="div__portfolio-parrafo">
+
+      </div>
+    </>
+  );
 }
-export default Portfolio
+export default Portfolio;
