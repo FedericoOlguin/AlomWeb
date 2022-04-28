6578bbc1d7ebc31f0cb10a1a1164849fa37d6768
montoyafranco <francomontoya9810@gmail.com>
2022-04-28 16:48:24 -0300

    creating css variables title and subtitle

--- frontend/src/Pages/Services/Services.js
+++ frontend/src/Pages/Services/Services.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "../Services/Services"
 import foto from "../../Media/HomePicture.svg"
+import ServicesHero from "../../Media/services-page.svg"
+import "./services.css"
 
 
 
@@ -10,17 +12,21 @@ const Services = () => {
 
 
 
-    return (
+    return (<>
         <div className="containerPrincipal">
             <div className="textContainerHero">
 
-                <h1>Átom</h1>
-                <h2 className="textoHero">Hola Soy un Services </h2>
+                <h1>Servicios</h1>
+                <h2 className="textoHero">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga fugiat ullam quos officiis? Est laboriosam consequatur inventore distinctio perferendis ipsa optio excepturi quidem, commodi adipisci. Iure odit sunt necessitatibus voluptas?</h2>
                 
             </div>
-                <img className="fotoFondo" src={foto} alt="Foto pc" />
+                <img className="fotoFondo" src={ServicesHero} alt="Foto pc" />
+        </div>
+        <div className="container__secundario__servicios">
+
         </div>
 
+        </>
     )
 }
 
