2df830d91b550535167816153993303228babeb9
Sebazt <juan.londono29@udea.edu.co>
2022-05-03 16:49:42 -0500

    Before modal

---
+++ frontend/src/components/ContactoHome/modal.jsx
@@ -0,0 +1,10 @@
+import React, {useState} from 'react'
+import { Modal, TextField, Button } from "@mui/material";
+
+function ModalComponente(){
+  return(
+    <>
+    </>
+  )
+}
+export default ModalComponente
