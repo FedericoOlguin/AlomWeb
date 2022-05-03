c67379e209af883bef76df8e3af7e01737e96f79
FedericoOlguin <fede_olguin_10@outlook.com.ar>
2022-05-03 18:27:38 -0300

    Service seccion

--- frontend/src/Pages/Home/HomePege.js
+++ frontend/src/Pages/Home/HomePege.js
@@ -24,6 +24,7 @@ const HomePage = () => {
                 <img className="homePage__fotoFondo" src={foto} alt="Foto pc" />
             </div>
             <ResAbout />
+            <ResServices/>
 
         </>
     )
--- frontend/src/components/ResServices/ResServices.jsx
+++ frontend/src/components/ResServices/ResServices.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./resServices.css"
 
 
 
@@ -6,11 +7,26 @@ import React from "react";
 
 function ResServices() {
     return (
-
-        <div>
-            <div>Div</div>
-            
-        </div>
+        <>
+            <h2 className="textCenter">Our ecosystem</h2>
+            <div className="serviceContainer">
+                <div className="serviceContainer__servicio">
+                    <h3>Titulo</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, similique facilis totam est omnis non.</p>
+                </div>
+                <div className="serviceContainer__servicio">
+                    <img className="serviceContainer__imagen" src="https://images.pexels.com/photos/8177853/pexels-photo-8177853.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" />
+                </div>
+                <div className="serviceContainer__servicio ">
+                    <img className="serviceContainer__imagen" src="https://images.pexels.com/photos/3473525/pexels-photo-3473525.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" />
+                </div>
+                <div className="serviceContainer__servicio">
+                    <h3>Titulo</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, similique facilis totam est omnis non.</p>
+                </div>
+            </div>
+        </>
 
     )
 }
+export default ResServices
